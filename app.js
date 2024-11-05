6a3855f806d6e941e642cfa3c6619e0b11296c2d
pctien <pctien1401>
2024-11-05 21:50:04 +0700

    feat(db):done entity + sample + connecting

--- app.js
+++ app.js
@@ -2,8 +2,6 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const path = require("path");
-const sequelize = require('./config/database');
-const User = require('./models/User');
 
 // Config template engine EJS
 app.set('view engine', 'ejs');
@@ -37,27 +35,8 @@ app.get('/contact', (req, res) => {
   res.render('layouts/layout', { body: '../contact/contact' });
 });
 
-
-// Sync db
-(async () => {
-  try {
-
-    await sequelize.sync({ force: false });
-    console.log("Database & tables sync!");
-
-    if(await User.count() == 0)
-    {
-      await User.create({
-        user_name: 'John Doe',
-        phone: '1234567890',
-        email: 'john@example.com',
-      });
-      console.log("Sample user created!");
-    }
-  } catch (error) {
-    console.error("Unable to sync database:", error);
-  }
-})();
+// db
+const sequelize = require('./models/index.js');
 
 
 // Start server
