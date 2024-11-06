bb97896a0e0ba6a8b45be9e9b97e8affdf39556a
C0smic <vovantung0212@gmail.com>
2024-11-06 14:17:51 +0700

    refactor(auth): authController renders view instead of returning json data

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
 
-router.get('/login', authController.login);
+router.get('/login', authController.getLogin);
 router.post('/login', authController.postLogin);
 router.get('/register', authController.getRegister);
 router.post('/register', authController.postRegister);
